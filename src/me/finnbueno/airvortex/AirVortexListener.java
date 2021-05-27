996903f1e7c454dbe6e26d0491b62fbe3563ce64
Finn Bon <finn.bueno@gmail.com>
2021-05-27 12:46:30 +0200

    initial commit

---
+++ src/me/finnbueno/airvortex/AirVortexListener.java
@@ -0,0 +1,27 @@
+package me.finnbueno.airvortex;
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.ability.CoreAbility;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+/**
+ * @author Finn Bon
+ */
+public class AirVortexListener implements Listener {
+
+	@EventHandler
+	public void onClick(PlayerInteractEvent event) {
+		if (event.getAction() != Action.LEFT_CLICK_AIR && event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+
+		Player player = event.getPlayer();
+		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+		if (bPlayer.canBend(CoreAbility.getAbility(AirVortex.class))) {
+			new AirVortex(player);
+		}
+	}
+
+}
